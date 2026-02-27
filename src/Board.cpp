46b69dc4252eb451020513f9da599b995a996413
Kurallevenn <kuratovenn@gmail.com>
2026-02-27 05:42:08 +0300

    Add Board class with unique_ptr storage

--- src/Board.cpp
+++ src/Board.cpp
@@ -3,3 +3,20 @@
 //
 
 #include "Board.h"
+#include <stdexcept>
+
+void Board::addEvent(std::unique_ptr<Event> event) {
+    cells.push_back(std::move(event));
+}
+
+Event& Board::getEvent(int position) {
+    if (position < 0 || position >= cells.size()) {
+        throw std::out_of_range("Position is out of board range");
+    }
+
+    return *cells[position];
+}
+
+int Board::size() const {
+    return static_cast<int>(cells.size());
+}
--- src/Board.h
+++ src/Board.h
@@ -5,8 +5,21 @@
 #ifndef GIT_BOARD_H
 #define GIT_BOARD_H
 
+#pragma once
+#include <vector>
+#include <memory>
+#include "Event.h"
 
 class Board {
+private:
+    std::vector<std::unique_ptr<Event>> cells;
+
+public:
+    Board() = default;
+
+    void addEvent(std::unique_ptr<Event> event);
+    Event& getEvent(int position);
+    int size() const;
 };
 
 
