9ffbad79470c3e1944e67e5ff45ac93a33ee774b
tumassa <tumasbest@gmail.com>
2026-03-07 19:06:12 +0300

    trap

---
+++ src/TrapEvent.h
@@ -0,0 +1,19 @@
+//
+// Created by Air on 07.03.2026.
+//
+
+#ifndef CONSOLE_FINANCE_GAME_TRAPEVENT_H
+#define CONSOLE_FINANCE_GAME_TRAPEVENT_H
+#include "Event.h"
+#include "Player.h"
+#include <string>
+
+class TrapEvent : public Event {
+private:
+    std::string title;
+public:
+    explicit TrapEvent(std::string t);
+    bool play(Player& player) override;
+    std::string getTitle() const override;
+};
+#endif //CONSOLE_FINANCE_GAME_TRAPEVENT_H
