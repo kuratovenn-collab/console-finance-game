1054d314671ef6f36beb8efb101f58225ae5863f
Kurallevenn <kuratovenn@gmail.com>
2026-03-09 23:43:47 +0300

    Significant changes in board initialization

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,23 +1,43 @@
+#include "UI.h"
 #include "Game.h"
 #include  "Dice.h"
+#include "GamblingEvent.h"
+#include "TrapEvent.h"
 #include <iostream>
-#include <cstdlib>
+#include <memory>
 
-Game::Game(const std::string& playerName)
-    : player(playerName)
-{
+#include <vector>
+#include <algorithm>
+#include <cstdlib>
+#include <random>
+
+Game::Game(const std::string& playerName) : player(playerName) {
     setupBoard();
 }
 void Game::setupBoard() {
-for (int i = 0; i < 12; ++i) {
-        board.addEvent(std::make_unique<GamblingEvent>("Tavern Gambling"));
-		board.addEvent(std::make_unique<TrapEvent>("Bandit Ambush"));
+    const int totalCells = 16;
+    std::vector<int> eventTypes;
+    for (int i = 0; i < totalCells; ++i) {
+        eventTypes.push_back(i % 6);
+    }
+
+    std::random_device rd;
+    std::mt19937 g(rd());
+    std::shuffle(eventTypes.begin(), eventTypes.end(), g);
+
+    for (int type : eventTypes) {
+        if (type == 0 || type == 2 || type == 4) {
+            board.addEvent(std::make_unique<GamblingEvent>("Tavern " + std::to_string(type)));
+        } else {
+            board.addEvent(std::make_unique<TrapEvent>("Danger " + std::to_string(type)));
+        }
     }
 }
 
+
 void Game::run() {
 
-    std::cout << "Game started!\n";
+    std::cout << BOLD << GREEN << "Game started!"<< RESET <<"\n";
 
     while (player.getPosition() < board.size()) {
 
@@ -26,6 +46,7 @@ void Game::run() {
         if (player.getLives() <= 0) {
             std::cout << "You lost all lives! Restarting from beginning...\n";
             ///player.reset();
+            break;
         }
     }
 
@@ -33,44 +54,39 @@ void Game::run() {
 }
 void Game::processTurn() {
 
+    std::cout << "\nPress Enter to roll dice...";
+    std::cin.get();
+    int roll = Dice::roll();
+    std::cout << "You rolled: " << BOLD << roll << RESET << "\n";
 
-   std::cout << "\nPress Enter to roll dice...";
-   std::cin.get();
-
-
-   int roll = Dice::roll();
-   std::cout << "You rolled: " << roll << "\n";
-
-
-   player.move(roll);
-
-
-   std::cout << "Current position: " << player.getPosition() << "\n";
+    player.move(roll);
+    std::cout << "Current position: " << player.getPosition() << "\n";
+    if (player.getPosition() >= board.size()) {
+        return;
+    }
 
 
-   if (player.getPosition() >= board.size()) {
-       return;
-   }
-
-
-   Event& event = board.getRandomEvent();
-   std::cout << "Event: " << event.getTitle() << "\n";
-   event.play(player);
+    Event& event = board.getEventAt(player.getPosition());
+    std::cout << "Event: " << BOLD << event.getTitle() << RESET << "\n";
+    event.play(player);
+    std::cout << "--- Player Stats ---\n";
+    std::cout << "Position: " << BLUE << player.getPosition() << RESET  << " /" << BLUE << " 16 " << RESET << "\n";
+    std::cout << "Money: " << YELLOW << player.getMoney() << RESET << "\n";
+    std::cout << "Lives: "  << RED << player.getLives() << RESET  << "\n";
+    std::cout << "--------------------\n";
 }
 void Game::checkEnding() {
-
-
-   int money = player.getMoney();
-
-
-   std::cout << "\n=== GAME OVER ===\n";
-
-
-   if (money < 50)
-       std::cout << "poor.\n";
-   else if (money < 150)
-       std::cout << "not poor.\n";
-   else
-       std::cout << "rich\n";
+    printLine();
+    if (player.getLives() <= 0) {
+        std::cout << RED << BOLD << "=====YOU DIED=====\n" "==YOUR ADVENTURE ENDS HERE==" << RESET << "\n";
+    } else {
+        std::cout << GREEN << BOLD << "===CONGRATULATIONS!===\n""==YOU REACHED THE END==" << RESET << "\n";
+        int money = player.getMoney();
+        if (money < 50)
+            std::cout << "poor.\n";
+        else if (money < 150)
+            std::cout << "not poor.\n";
+        else
+            std::cout << "rich\n";
+    }
 }
-
