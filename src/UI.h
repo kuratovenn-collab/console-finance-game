dca5f3d2cc77979964ab83dded56eb85b14ae5bf
Kurallevenn <kuratovenn@gmail.com>
2026-03-09 23:44:05 +0300

    Add ANSI colors and improved console UI

---
+++ src/UI.h
@@ -0,0 +1,25 @@
+//
+// Created by ASUS on 09.03.2026.
+//
+#ifndef UI_H
+#define UI_H
+
+#include <iostream>
+#include <string>
+
+const std::string RESET  = "\033[0m";
+const std::string RED    = "\033[31m";
+const std::string GREEN  = "\033[32m";
+const std::string YELLOW = "\033[33m";
+const std::string BLUE   = "\033[34m";
+const std::string BOLD   = "\033[1m";
+
+inline void printLine() {
+    std::cout << BOLD << "------------------------------------------" << RESET << "\n";
+}
+
+inline void clearConsole() {
+    for (int i = 0; i < 10; ++i) std::cout << "\n";
+}
+
+#endif
