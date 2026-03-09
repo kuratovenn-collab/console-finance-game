fef4a3dadca656ecd7541c7c0e6b2549a97adcb2
Kurallevenn <kuratovenn@gmail.com>
2026-03-09 23:42:38 +0300

    Changes in board-based event system

--- src/Board.cpp
+++ src/Board.cpp
@@ -3,23 +3,19 @@
 //
 
 #include "Board.h"
+#include <memory>
 #include <stdexcept>
 
 void Board::addEvent(std::unique_ptr<Event> event) {
     events.push_back(std::move(event));
 }
-
-Event& Board::getRandomEvent() {
-	if (events.empty()) {
-		throw std::runtime_error("No events on board");
+Event& Board::getEventAt(int index) {
+	if (index >= events.size()) {
+		return *events.back();
 	}
-	int newID;
-	do {
-		newID = std::rand() % events.size();
-	} while (newID == lastEventID);
-	lastEventID = newID;
-	return *events[newID];
+	return *events[index];
 }
+
 int Board::size() const {
     return static_cast<int>(events.size());
 }
