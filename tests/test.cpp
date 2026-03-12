df1f3400404e007e7515cc8cbc9c458e288bfaf3
Kurallevenn <kuratovenn@gmail.com>
2026-03-13 01:18:48 +0300

    Add tests, Implement GoogleTests

---
+++ tests/test.cpp
@@ -0,0 +1,35 @@
+#include <gtest/gtest.h>
+
+#include "../src/Player.h"
+#include "../src/Dice.h"
+
+TEST(PlayerTest, AddMoney) {
+
+    Player p("Hero",100);
+    p.addMoney(50);
+    EXPECT_EQ(p.getMoney(),150);
+}
+
+TEST(PlayerTest, SpendMoney) {
+
+    Player p("Hero",100);
+    p.spendMoney(30);
+    EXPECT_EQ(p.getMoney(),70);
+}
+
+TEST(PlayerTest, Move) {
+
+    Player p("Hero");
+    p.move(5);
+    EXPECT_EQ(p.getPosition(),5);
+}
+
+TEST(DiceTest, Range) {
+
+    for(int i=0;i<100;i++){
+
+        int r = Dice::roll();
+        EXPECT_GE(r,1);
+        EXPECT_LE(r,6);
+    }
+}
